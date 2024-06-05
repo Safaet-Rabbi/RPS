97b1c4eed4d57519caffc649dd9c1fbaa07e6ccc
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-06-05 17:12:51 +0600

    Update script.js

--- script.js
+++ script.js
@@ -12,35 +12,24 @@ optionImages.forEach((image, index) => {
     result.textContent = "Wait...";
 
     optionImages.forEach((image2, index2) => {
-      // If the current index doesn't match the clicked index
-      // Remove the "active" class from the other option images
       index !== index2 && image2.classList.remove("active");
     });
 
     gameContainer.classList.add("start");
 
-    // Set a timeout to delay the result calculation
     let time = setTimeout(() => {
       gameContainer.classList.remove("start");
 
-      // Get the source of the clicked option image
       let imageSrc = e.target.querySelector("img").src;
-      // Set the user image to the clicked option image
       userResult.src = imageSrc;
 
-      // Generate a random number between 0 and 2
       let randomNumber = Math.floor(Math.random() * 3);
-      // Create an array of CPU image options
       let cpuImages = ["images/rock.png", "images/paper.png", "images/scissors.png"];
-      // Set the CPU image to a random option from the array
       cpuResult.src = cpuImages[randomNumber];
 
-      // Assign a letter value to the CPU option (R for rock, P for paper, S for scissors)
       let cpuValue = ["R", "P", "S"][randomNumber];
-      // Assign a letter value to the clicked option (based on index)
       let userValue = ["R", "P", "S"][index];
 
-      // Create an object with all possible outcomes
       let outcomes = {
         RR: "Draw",
         RP: "Cpu",
@@ -56,7 +45,6 @@ optionImages.forEach((image, index) => {
       // Look up the outcome value based on user and CPU options
       let outComeValue = outcomes[userValue + cpuValue];
 
-      // Display the result
       result.textContent = userValue === cpuValue ? "Match Draw" : `${outComeValue} Won!!`;
     }, 2500);
   });
